9e27046e694e77464af0eecf47076ae8c2023ffe
HardyValen <hardyvalen@gmail.com>
2021-02-28 07:10:25 +0700

    created branch build

--- src/components/_outlet-slider-content.js
+++
@@ -1,9 +0,0 @@
-const SliderContent = ({data, id}) => {
-  return data 
-    ? <div className="outlet-slider-content" id={id}>
-        <img src={data?.image} alt=""/>
-      </div> 
-    : null
-}
-
-export default SliderContent
